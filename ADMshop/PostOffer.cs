3c353d0a053b9df4b676311aee363c1b908a4be6
Me <Me@DESKTOP-HUPCLDV>
2021-03-14 14:47:09 +0200

    Vajni promeni po ofertata

---
+++ ADMshop/PostOffer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ADMshop
+{
+    public partial class PostOffer : Form
+    {
+        public PostOffer()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = new Bitmap(open.FileName);
+                
+            }
+        }
+    }
+}
